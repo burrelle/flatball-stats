d7cf5de9fa7ef29d45d005bb09deccc52b8f4131
Evan Burrell <burrelle@oregonstate.edu>
2019-04-07 21:10:29 +0100

    Individual games

--- resources/views/game/show.blade.php
+++ resources/views/game/show.blade.php
@@ -1,2 +1,12 @@
-{{ $game->opponent }}
-{{ $game->tournament }}
+@extends('layouts.app') @section('content')
+<div class="flex items-center flex-col">
+    <h2>
+        {{ $game->team->name }} vs. {{ $game->opponent }} @
+        {{ $game->tournament }}
+    </h2>
+    <h3>{{ $game->score }} - {{ $game->opponent_score }}</h3>
+</div>
+
+@foreach($game->team->players as $player)
+<p>{{ $player->name }}</p>
+@endforeach @endsection
